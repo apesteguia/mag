335ef698a0612e8100e27e08b0f454166b5d8faf
apesteguia <mikeltxomikeltxo@gmail.com>
2024-09-17 14:39:01 +0200

    first commit

---
+++ src/filesys.rs
@@ -0,0 +1,138 @@
+use std::{
+    fs::{self, File, Metadata},
+    os::unix::fs::FileExt,
+    path::{Path, PathBuf},
+};
+
+const FILE_READ_BYTES: usize = 256;
+
+#[derive(Debug, Clone)]
+pub struct MagItem {
+    pub path: PathBuf,
+    pub metadata: Option<Metadata>,
+}
+
+impl MagItem {
+    pub fn new<P: AsRef<Path>>(path: P) -> Self {
+        let path = path.as_ref().to_owned();
+        let metadata = match fs::metadata(&path) {
+            Ok(meta) => Some(meta),
+            Err(_) => None,
+        };
+        Self { path, metadata }
+    }
+
+    pub fn from<P: AsRef<Path>>(path: P, metadata: Option<Metadata>) -> Self {
+        let path = path.as_ref().to_owned();
+
+        Self { path, metadata }
+    }
+}
+
+#[derive(Debug, Clone)]
+pub struct MagFile {
+    pub data: MagItem,
+}
+
+impl MagFile {
+    pub fn new<P: AsRef<Path>>(path: P) -> Self {
+        let path = path.as_ref().to_owned();
+        let data = MagItem::new(&path);
+
+        Self { data }
+    }
+
+    pub fn file_contents(&self) -> String {
+        let mut buf = [0u8; FILE_READ_BYTES];
+        let file = match File::open(&self.data.path) {
+            Ok(f) => f,
+            Err(e) => return format!("{} {}", "can't open file".to_string(), e),
+        };
+
+        match file.read_at(&mut buf, 0) {
+            Ok(_) => String::from_utf8_lossy(&buf).to_string(),
+            Err(e) => return format!("{} {}", "can't open file".to_string(), e),
+        }
+    }
+}
+
+#[derive(Debug, Clone)]
+pub struct MagFolder {
+    pub data: MagItem,
+}
+
+impl MagFolder {
+    pub fn new<P: AsRef<Path>>(path: P) -> Self {
+        let path = path.as_ref().to_owned();
+        let data = MagItem::new(&path);
+
+        Self { data }
+    }
+
+    pub fn get_entries(&self) -> Option<Vec<MagEntry>> {
+        let mut v: Vec<MagEntry> = Vec::new();
+
+        let entries = match fs::read_dir(&self.data.path) {
+            Ok(entries) => entries,
+            Err(_) => return None,
+        };
+
+        for entry in entries {
+            let entry = match entry {
+                Ok(e) => e,
+                Err(_) => return None,
+            };
+            let path = entry.path();
+
+            let metadata = match fs::metadata(entry.path()) {
+                Ok(m) => Some(m),
+                Err(_) => None,
+            };
+
+            if let Some(metadata) = metadata {
+                if metadata.is_file() {
+                    v.push(MagEntry::File(MagFile::new(&path)));
+                } else {
+                    v.push(MagEntry::Dir(MagFolder::new(&path)));
+                }
+            }
+        }
+
+        Some(v)
+    }
+}
+
+#[derive(Debug, Clone)]
+pub enum MagEntry {
+    Dir(MagFolder),
+    File(MagFile),
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_file() {
+        let f = MagFile::new("/home/mikel/Escritorio/rust/mag/Cargo.toml");
+        let s = f.file_contents();
+        println!("{s}");
+        assert!(s.len() > 0);
+    }
+
+    #[test]
+    fn test_dir() {
+        let d = MagFolder::new("/home/mikel/");
+        let entries = match d.get_entries() {
+            Some(v) => v,
+            None => Vec::new(),
+        };
+
+        for entry in entries {
+            match entry {
+                MagEntry::Dir(folder) => println!("Directory: {:?}", folder.data.path),
+                MagEntry::File(file) => println!("File: {:?}", file.data.path),
+            }
+        }
+    }
+}
---
+++ src/main.rs
@@ -0,0 +1,7 @@
+pub mod filesys;
+pub mod state;
+pub mod ui;
+
+fn main() {
+    println!("Hello, world!");
+}
